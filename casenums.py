2ef686749841a2c8c1ad464e74693fa2f3095538
scottschmidl <sjschmidl@gmail.com>
2022-03-05 15:30:21 -0600

    added doc strings

--- casenums.py
+++ casenums.py
@@ -3,11 +3,28 @@ import pandas as pd
 class UniqueDataFrames:
 
     def __init__(self, notes_file, feature_file):
+        """__init__ initialize class with filename
+
+        Args:
+            notes_file (string): filenname of first df
+            feature_file (string): filename of second df
+        """
 
         self.patient_notes = pd.read_csv(notes_file)
         self.features = pd.read_csv(feature_file)
 
     def merge_dfs(self, notes_col="case_num", features_col1="feature_num", features_col2="feature_text", notes_featues_col="features"):
+        """merge_dfs merge the patient notes and features dfs
+
+        Args:
+            notes_col (str, optional): column in notes on which to merge and the first column in new df created from dictionary d. Defaults to "case_num".
+            features_col1 (str, optional): key for dictionary. Defaults to "feature_num".
+            features_col2 (str, optional): value of dictionary. Defaults to "feature_text".
+            notes_featues_col (str, optional): name of new column created by the dictionary d. Defaults to "features".
+
+        Returns:
+            pandas dataframe: the new merged dataframe of patient notes and features
+        """
 
         d = {}
         for i in self.features[notes_col].unique():
@@ -19,5 +36,14 @@ class UniqueDataFrames:
         return self.patient_notes.merge(right=features_dict, left_on="case_num", right_on="case_num")
 
     def each_dataframe(self, notes_features, notes_name="case_num"):
+        """each_dataframe creates a dicationary of separate dataframes based off of unqiue case numbers
+
+        Args:
+            notes_features (pandas dataframe): the dataframe used to create the new dataframe
+            notes_name (str, optional): column to use to get unqiue keys. Defaults to "case_num".
+
+        Returns:
+            dict: dictionary where the key is a case number and the value is the dataframe specific to that case number
+        """
 
         return {case_num: notes_features[notes_features[notes_name] == case_num] for case_num in notes_features[notes_name].unique()}
